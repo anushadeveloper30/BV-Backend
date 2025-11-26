da10f9e8cb09c3c8433e3227a4b095ac92490e58
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-11-26 14:06:17 +0500

    JsonWebToken

--- controllers/auth.conrollers.js
+++ controllers/auth.conrollers.js
@@ -15,21 +15,21 @@ export const signup = async (req, res) => {
         const user = await User.findOne({ email });
         if (user) return res.status(400).json({
             success: false,
-            message: "user already exits"
+            message: "User already exists"
         });
 
         await User.create({ firstName, lastName, email, password });
 
         res.status(201).json({
             success: true,
-            message: "Registration Successfull"
+            message: "User registered successfully"
         });
     } catch (error) {
         res.status(400).json({
             success: false,
-            message: "Internal Server Error"
+            message: error.message || "Internal Server Error",
         });
-        console.log(error);
+        console.log(error.message);
     }
 }
 
@@ -37,6 +37,7 @@ export const signup = async (req, res) => {
 export const login = async (req, res) => {
     try {
         const { email, password } = req.body;
+
         if (!email || !password) return res.status(400).json({
             success: false,
             message: "Invalid Email Or Password"
@@ -58,7 +59,7 @@ export const login = async (req, res) => {
 
         const accessToken = generateAccessToken(user);
 
-        res.cookie("XntricToken", accessToken, {
+        res.cookie("BlueVerseToken", accessToken, {
             httpOnly: true,
             secure: true,
             sameSite: process.env.NODE_ENV === "production" ? "None" : "lax",
@@ -71,10 +72,10 @@ export const login = async (req, res) => {
             user,
         });
     } catch (error) {
-        console.log(error);
+        console.log(error.message || "Internal Server Error");
         res.status(400).json({
             success: false,
-            message: "Internal Server Error"
+            message: error.message || "Internal Server Error"
         });
     }
 }
@@ -82,16 +83,16 @@ export const login = async (req, res) => {
 // logout Api
 export const logOut = async (req, res) => {
     try {
-        await res.clearCookie("XntricToken");
+        await res.clearCookie("BlueVerseToken");
         res.status(200).json({
             success: true,
             message: "Logout Successfull"
         })
     } catch (error) {
-        console.log(error);
         res.status(400).json({
             success: false,
-            message: "Internal Server Error"
+            message: error.message || "Internal Server Error"
         });
+        console.log(error.message || "Internal Server Error");
     };
 };
