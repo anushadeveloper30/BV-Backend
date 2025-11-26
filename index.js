479700ffa13ffed4563d357c16cffd2e864a011e
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-11-26 11:56:14 +0500

    Mongodb Connection With Server Listening

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 import express from "express";
 import { Welcome } from "./helpers/welcome.js";
 import "dotenv/config";
+import { connectDB } from "./database/connection.js";
 
 const app = express();
 const port = process.env.PORT;
 
 app.get("/", Welcome);
 
-app.listen(port, () => {
-    console.log(`Server is running on the port ${port}`)
-})
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Server is running on the port ${port}`)
+    })
+}).catch((error) => {
+    console.error("Server failed to start:", error.message);
+    process.exit(1);
+});
