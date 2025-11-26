731b83c09fa92ae1ebdda764e4e7656470e28158
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-11-26 13:55:15 +0500

    Customer Model And Invoice

--- models/customer.model.js
+++ models/customer.model.js
@@ -1,39 +1,38 @@
 // models/Customer.js
 import mongoose from "mongoose";
 
-const customerSchema = new mongoose.Schema(
-    {
-        user: {
-            type: String,
-            required: true,
-        },
-        firstName: {
-            type: String,
-            required: true
-        },
-        lastName: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true
-        },
-        phoneNumber: {
-            type: String,
-            required: true
-        },
-        licencePlateNumber: {
-            type: String,
-            required: true
-        },
-        address: {
-            type: String,
-        },
-        state: {
-            type: String,
-            default: "Al Quoz"
-        },
-    }, { timestamps: true });
+const customerSchema = new mongoose.Schema({
+    user: {
+        type: String,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,
+        required: true
+    },
+    licencePlateNumber: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+    },
+    state: {
+        type: String,
+        default: "Al Quoz"
+    },
+}, { timestamps: true });
 
 export default mongoose.model("Customer", customerSchema);
---
+++ models/invoice.models.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+
+const invoiceSchema = new mongoose.Schema({
+        customer: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Customer",
+            required: true,
+        },
+        serviceDetails: {
+            id: String,
+            serviceName: {
+                type: String,
+                required: true
+            },
+            type: String,
+            price: {
+                type: Number,
+                required: true
+            },
+        },
+        state: {
+            type: String,
+            default: "Al Quoz"
+        },
+        discounts: {
+            type: Number,
+            default: 0
+        }, 
+        subtotal: {
+            type: Number,
+            required: true
+        },
+        taxRate: {
+            type: Number,
+            default: 0.05
+        },
+        taxAmount: {
+            type: Number,
+            required: true
+        },
+        total: {
+            type: Number,
+            required: true
+        },
+    }, { timestamps: true });
+
+    
+// Tax & total Calculating From Backend
+invoiceSchema.pre("validate", function (next) {
+    const price = this.serviceDetails?.price || 0;
+    const discount = this.discounts || 0;
+
+    const subtotal = price - discount;
+    const taxAmount = subtotal * this.taxRate;
+    const total = subtotal + taxAmount;
+
+    this.subtotal = subtotal;
+    this.taxAmount = taxAmount;
+    this.total = total;
+
+    next();
+});
+
+export default mongoose.model("Invoice", invoiceSchema);
