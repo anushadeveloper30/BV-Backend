0a846dbf2bfd549cdf25eeb62a664804b960ba4f
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-11-26 16:44:33 +0500

    get Customer

--- controllers/customer.controllers.js
+++ controllers/customer.controllers.js
@@ -33,3 +33,24 @@ export const createCustomer = async (req, res) => {
         });
     }
 }
+
+export const getCustomer = async (req, res) => {
+    try {
+        const { customerId } = req.params;
+        const user = await User.findOne({ customerId });
+        if (!user) return res.status(404).json({
+            success: false,
+            message: "Customer not found"
+        });
+        res.status(200).json({
+            success: true,
+            user
+        });
+    } catch (error) {
+        console.error("Error getting customer:", error.message);
+        res.status(500).json({
+            success: false,
+            message: error.message || "Internal server error",
+        });
+    }
+}
